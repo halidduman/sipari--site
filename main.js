91663d7e8e4a61d306be6492307a1deca2a204f4
halidduman <halidduman60@gmail.com>
2024-06-06 01:06:56 +0300

    u-1

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuIcon = document.getElementById('menu-icon-h');
-  const navbar = document.querySelector('.navbar-h');
-
-  menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('open-menu-h');
-  });
-});
