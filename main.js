0577f212286d95f0a866848095868fc70dbb09f3
halidduman <halidduman60@gmail.com>
2024-06-05 18:48:40 +0300

    slider-boyutlandırılacak

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const menuIcon = document.getElementById('menu-icon');
-  const navbar = document.querySelector('.navbar');
+  const menuIcon = document.getElementById('menu-icon-h');
+  const navbar = document.querySelector('.navbar-h');
 
   menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('open-menu');
+    navbar.classList.toggle('open-menu-h');
   });
 });
